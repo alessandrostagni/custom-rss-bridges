d51cb53578f0b912fc933256180496f0cd52b96c
alessandrostagni <darth.sidius92@gmail.com>
2023-09-02 02:06:32 +1200

    DigimonCard.io Bridge working fine with decks :3. Added README instructions for using phantomJS (hopefully won't be needed).

---
+++ DigimonCardIoBridge.php
@@ -0,0 +1,77 @@
+<?php
+//require __DIR__ . '/../vendor/autoload.php';
+//use JonnyW\PhantomJs\Client;
+
+class DigimonCardIoBridge extends BridgeAbstract
+{
+    const NAME = 'DigimonCard.io';
+    const URI = 'https://digimoncard.io';
+    const DESCRIPTION = 'digimoncard.io bridge';
+    const MAINTAINER = 'alessandrostagni';
+    const COLOR_MAPPING = array(
+    'Red' => '#ee2d48',
+    'Purple' => '#af51d6',
+    'Blue' => '#157ffb',
+    'Black' => '#2c2d2d',
+    'Yellow' => '#c4cd35',
+    'Green' => '#30c12e',
+    'White' => '#c7c7c7'
+    );
+    
+    private function getDeckList($url) {
+	$elements_to_remove = array();    
+	$dom = getSimpleHTMLDOM($url);
+	$deckList = $dom->find('body', 0)->find('main', 0)->find('div[class=container]',0)->find('div[class=info-area]',0)->find('div[id=full-deck]', 0);
+	$mainDeck = $deckList->find('div[id=main_deck]', 0);
+	$mainDeck->setAttribute('style', '');
+	foreach($mainDeck->find('div[class=cardGroupSingle]') as $cardGroup){
+	    $deckCountNumber = $cardGroup->find('div[class=deckcountnumber]', 0);
+	    $a = $cardGroup->find('a', 0);
+	    $cardId = str_replace('/card/?search=', '', $a->href);
+	    $deckCountNumber->innertext = $deckCountNumber->innertext.' '.$cardId;
+	    $a->setAttribute('href', self::URI.$a->href);
+	    $img = $a->find('img');
+	    $img[0]->setAttribute('src', $img[0]->getAttribute('data-src'));
+	    $img[1]->outertext = '';
+	}
+	
+	$eggDeck = $deckList->find('div[id=egg_deck]', 0);
+	$eggDeck->setAttribute('style', '');
+        foreach($eggDeck->find('span[class=img-container]') as $cardGroup){
+            $img = $cardGroup->find('a', 0)->find('img', 0);
+	    $img->setAttribute('src', $img->getAttribute('data-src'));
+	}
+	$sideDeck = $deckList->find('div[id=side_deck]', 0);
+	$sideDeck->setAttribute('style', '');
+	foreach($sideDeck->find('span[class=img-container]') as $cardGroup){
+            $img = $cardGroup->find('a', 0)->find('img', 0);
+            $img->setAttribute('src', $img->getAttribute('data-src'));
+        }
+	$deckList->find('div[class=deck-breakdown]', 0)->outertext='';
+	return $deckList;
+    }
+    public function collectData()
+    {
+	$url = self::URI;
+    	$dom = getSimpleHTMLDOM($url);
+	$dom = $dom->find('body', 0)->find('main', 0)->find('div[class=container]', 0)->find('div[id=latest-decks]', 0)->find('div[class="deck-layout-flex"]', 0);
+	foreach ($dom->find('div[class="deck-layout-single-flex"]') as $deck) {
+	    $color = $deck->find('span[class=ribbon-deck-text]', 0)->innertext;	
+	    $color = self::COLOR_MAPPING[$color];
+	    $a = $deck->find('a', 0);
+	    $uri = $url.$a->href;
+	    $deckList = $this->getDeckList($uri);
+	    $div = $a->find('div', 0);
+	    $imgUri = $div->getAttribute('data-src');
+	    $entryTitle = $div->find('h4', 0);
+	    $to_remove = $entryTitle->find('span', 0);
+	    $to_remove->outertext='';
+	    $this->items[] = [
+		 'title' => $entryTitle->innertext,   
+		 'uri' => $uri,
+		 'content' => "<a href=\"$uri\"><img align=center src=\"$imgUri\"></img></a><h1><font color=\"$color\">$entryTitle</font></h1>$deckList"
+	    ];
+	}
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # custom-rss-bridges
 A collection of custom rss bridges for [RSS Bridge](https://github.com/RSS-Bridge/rss-bridge)
+
+## How to use PhantomJS in your PHP bridges
+
+Sometimes you need to wait for pages to be fully loaded in order to parse them correctly.
+This is not possible by using PHP natively, as it is a server side language.
+
+One option is to use the headless PhantomJS browser, along with the PHP interface that's been developed for it.
+
+I managed to install PhantomJS correctly on Amazon Linux AMI 2.
+I had to use [composer 1.9](https://getcomposer.org/download/1.9.0/composer.phar) to install the correct version of PhantomJS and the PHP interface by following these [instructions](https://jonnnnyw.github.io/php-phantomjs/)
+
+For reference, here is an example snippet. <br/>
+Specifically, you can read these [docs](https://jonnnnyw.github.io/php-phantomjs/4.0/3-usage/#on-load-finished) on how to wait for a page to be fully loaded.
+
+```
+$client = Client::getInstance();
+$client->isLazy();
+$client->getEngine()->setPath('"PATH_TO_YOUR_PHANTOMJS_EXECUTABLE");
+$client->getEngine()->addOption('--ssl-protocol=any');
+$client->getEngine()->addOption('--ignore-ssl-errors=true');
+$request = $client->getMessageFactory()->createRequest('https://google.com');
+$request->setTimeout(5000);
+$request->setRequestData(array('offset' => '10'));
+$response = $client->getMessageFactory()->createResponse();
+$client->send($request, $response);
+$dom = str_get_html($response->getContent());
+```
+
+I **strongly** recommend of testing HTTP and HTTPS requests through the use of an [example script](https://github.com/ariya/phantomjs/blob/master/examples/post.js).
+99% of the time PHP won't throw any error and just show an empty string as value returned by any of the PhantomJS calls.
+
+### Note
+
+PhantomJS is [no longer maintained](https://www.puzzle.ch/de/blog/articles/2018/02/12/phantomjs-is-dead-long-live-headless-browsers#:~:text=As%20of%20spring%202017%2C%20PhantomJS,PhantomJS%2C%20the%20headless%20WebKit%20browser.), so if you have better suggestions available for PHP that are not too heavy for a small self-hosted service, feel free to open an Issue or even a PR ;).
